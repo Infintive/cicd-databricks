f9191e9b4865ff6e9ca28fb7bb6dea3a395e7884
Tyler Tracey <tyler.tracey@infinitive.com>
2026-03-04 14:23:04 -0500

    silver

--- bundles/resorts/src/transformations/bronze/bronze.py
+++ bundles/resorts/src/transformations/bronze/bronze.py
@@ -4,10 +4,9 @@ catalog = spark.conf.get("catalog_name")
 schema = spark.conf.get("schema_name")
 volume = spark.conf.get("volume_name")
 
-print(f"{catalog}, {schema}, {volume}")
-
-@dlt.table
-def resorts():
+@dlt.table(name="resorts_bronze")
+def resorts_bronze():
+  """Load data into the bronze table using Auto Loader."""
   return spark.readStream.format("cloudFiles") \
     .option("cloudFiles.format", "csv") \
     .load(f"/Volumes/{catalog}/{schema}/{volume}/")
